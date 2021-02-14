747857584ce2b912e363099e078853fcaefb5a4d
omerdemiralay <74465397+omerdemiralay@users.noreply.github.com>
2021-02-14 20:45:39 +0300

    Add files via upload

---
+++ FormGroup/script.js
@@ -0,0 +1,8 @@
+
+function Reset(){
+    document.getElementById('name').value='';
+    document.getElementById('email').value='';
+    document.getElementById('password').value='';
+    document.getElementById('tel').value='';
+
+}
